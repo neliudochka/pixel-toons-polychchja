e59af7959431d51eb3ba840e0a3f3b9456899715
Mila <gorbunova.milasha@gmail.com>
2022-06-02 22:28:33 +0300

    fixed some html problems

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,9 @@
 window.onload = () => {
   console.log('Example loaded successfully!');
 };
+
+//canvas
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+context.fillStyle = 'red';
+context.fillRect(100, 10, 100, 50);

ec9b666a3fbf627aaad791bdbe64ce6ed00b220b
Mila <gorbunova.milasha@gmail.com>
2022-06-04 03:57:04 +0300

    лютий хард/гавнокод, що працює

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,23 +1,60 @@
-/*
-window.onload = () => {
-  console.log('Example loaded successfully!');
+const pixelSize = 20;
+const brushColor = '#0000FF';
+
+let canvas = document.createElement('canvas');
+let canvasContainer = document.getElementById('canvas-container');
+canvasContainer.appendChild(canvas);
+
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'green';
+
+const picture = {
+  height: 5,
+  width: 5,
+  pixels: ['#000000', '#000000', '#FFFFFF','#000000','#FFFFFF','#000000','#FFFFFF','#000000','#FFFFFF'],
+  pixel(x, y){
+    return this.pixels[x + y * this.width];
+  }
 };
 
-
-  let state = {
-    tool: "draw",
-    color: "#000000",
-    picture: Picture.empty(60, 30, "#f0f0f0")
-  };
-  let app = new PixelEditor(state, {
-    tools: {draw, fill, rectangle, pick},
-    controls: [ToolSelect, ColorSelect],
-    dispatch(action) {
-      state = updateState(state, action);
-      app.syncState(state);
+function createCanvas (picture, canvas) {
+  canvas.width = picture.width * pixelSize;
+  canvas.height = picture.height * pixelSize;
+  canvas.style.backgroundColor = 'green';
+  for (let y = 0; y < picture.height; y++) {
+    for (let x = 0; x < picture.width; x++) {
+      ctx.fillStyle = picture.pixel(x, y);
+      ctx.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
     }
-  });
-  document.querySelector("div").appendChild(app.dom);
-*/
-//canvas
+  }
+}
+
+createCanvas(picture, canvas);
+
+console.log(canvas.getBoundingClientRect());
+
+canvas.addEventListener("mousedown", getMousePosition);
+
+
+
+function getMousePosition (event) {
+  const canPos = canvas.getBoundingClientRect();
+  console.log(canPos);
+  const coord = {
+    x: Math.floor((event.clientX-canPos.left)/pixelSize),
+    y: Math.floor((event.clientY-canPos.top)/pixelSize)
+  };
+  console.log(coord)
+  changePixelColor(coord.x, coord.y);
+}
+
+function changePixelColor (x, y) {
+  picture.pixels[x + y * picture.width] = brushColor;
+  ctx.fillStyle = picture.pixel(x, y);
+  ctx.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
+  console.log(picture.pixels);
+}
+
+
+
 

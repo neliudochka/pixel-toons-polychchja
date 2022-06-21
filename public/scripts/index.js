c0bdda3c5819293c7015b7f90f6eee8b6078920e
Mila <gorbunova.milasha@gmail.com>
2022-06-22 02:49:14 +0300

    виправила проблему накладання канвасів

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,10 +4,10 @@ import { Application } from './application.js';
 
 window.onload = () => {
   ee.on('newOptions', (data) => {
-    /*    const canvas = document.getElementsByTagName('canvas');
+    const canvas = document.getElementsByTagName('canvas');
     //очищую, щоб уникнути накладання кількох канвасів
     if (canvas.length > 0) canvas.item(0).remove();
-  */
+
     const shit = new Application(new Options(data));
     console.log('shit', shit);
   });
